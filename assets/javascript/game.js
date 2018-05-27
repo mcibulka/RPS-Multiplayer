606a9af1db9940df38bfe9f303427388fe82ca66
Matthew Cibulka <mcibulkadev@gmail.com>
2018-05-26 23:23:24 -0400

    Added 2 columns to display player + opponent hands, amd images for player to select their hand.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,15 @@
 $( document ).ready( function() {
-    console.log( "READY!" );
+    var $playerImg = $( '#player-img-sel' );
+
+    $playerImg.on("click", ".player-img", function() {
+        var name = ($(this).attr("data-name"));
+
+        $( "#player-choice" ).toggleClass( "fa-hand-" + name );
+        $( "#player-choice" ).toggleClass( "fa-camera-retro" );
+        // $( "#player-choice" ).toggleClass( "fa-camera-retro" );
+        console.log(name);
+    });
 });
+
+
+
